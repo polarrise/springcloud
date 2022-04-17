43de04e8b51702430bb062e3387935b6a45f32db
polarrise <1585914314@qq.com>
2022-04-17 16:25:04 +0800

    rise

---
+++ hystrix-dashboard/src/test/java/cn/how2j/springcloud/ProductServiceHystrixDashboardApplicationTest.java
@@ -0,0 +1,20 @@
+package cn.how2j.springcloud;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ProductServiceHystrixDashboardApplication.
+ */
+public class ProductServiceHystrixDashboardApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ springAop-study/src/main/java/cn/how2j/springcloud/lambdas/methodReference/DemoPrint.java
@@ -0,0 +1,10 @@
+package cn.how2j.springcloud.lambdas.methodReference;
+
+/**
+ * @author Jinbiao
+ * @date 2022/4/9
+ * @apiNote
+ */
+public class DemoPrint {
+
+}
