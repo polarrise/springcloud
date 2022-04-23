70a02fcfc1c57305b47c9f6e835e2ce3a461c10b
polarrise <1585914314@qq.com>
2022-04-23 16:49:35 +0800

    原型模式学习

---
+++ springAop-study/src/main/java/cn/how2j/springcloud/javaStudy/StringUtilsTest.java
@@ -0,0 +1,17 @@
+package cn.how2j.springcloud.javaStudy;
+
+import org.apache.commons.lang.StringUtils;
+
+public class StringUtilsTest {
+    public static void main(String[] args) {
+        String str="";
+        //字符串是空字符串或者null则 返回true
+        boolean result = StringUtils.isBlank(str);
+        System.out.println(result);
+
+        //字符串不是空字符串或者不为null则 返回true
+        String str2=null;
+        boolean result2 = StringUtils.isNotBlank(str);
+        System.out.println(result2);
+    }
+}
